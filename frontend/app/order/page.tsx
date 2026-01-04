7e58a235178a18908e0ffac6976bdabf8ebbbcde
Suleiman Zhukov <suleimanzhukov@gmail.com>
2026-01-04 09:21:54 +0300

    edit order page

--- frontend/app/order/page.tsx
+++ frontend/app/order/page.tsx
@@ -101,14 +101,12 @@ export default function Order() {
     orderItemsCount: number,
     orderId: number
   ) => {
-    console.log("AAAAAAA#######————#########AAAAAAAAA", count, orderItemsCount);
-    if (!count) return deleteItemMutation.mutate({ id });
     if (!count && orderItemsCount === 1) {
-      console.log("HERE");
       deleteItemMutation.mutate({ id });
       deleteOrderMutation.mutate({ id: orderId });
       return;
     }
+    if (!count) return deleteItemMutation.mutate({ id });
     mutation.mutate({ id, count });
   };
 
